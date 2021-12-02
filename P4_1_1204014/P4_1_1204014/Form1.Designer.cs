59c8e64c06a4f85797b88c062d08e1cce97558ea
anitaalfisyahra <anitasyahra17@gmail.com>
2021-12-02 14:45:20 +0700

    Tugas_Pemrograman_4

---
+++ P4_1_1204014/P4_1_1204014/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace P4_1_1204014
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vbButton = new System.Windows.Forms.Button();
+            this.csButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // vbButton
+            // 
+            this.vbButton.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vbButton.ForeColor = System.Drawing.Color.Green;
+            this.vbButton.Location = new System.Drawing.Point(34, 90);
+            this.vbButton.Name = "vbButton";
+            this.vbButton.Size = new System.Drawing.Size(75, 23);
+            this.vbButton.TabIndex = 0;
+            this.vbButton.Text = "Visual Basic";
+            this.vbButton.UseVisualStyleBackColor = true;
+            this.vbButton.Click += new System.EventHandler(this.vbButton_Click);
+            this.vbButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.vbButton_MouseMove);
+            // 
+            // csButton
+            // 
+            this.csButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.csButton.ForeColor = System.Drawing.Color.Red;
+            this.csButton.Location = new System.Drawing.Point(156, 90);
+            this.csButton.Name = "csButton";
+            this.csButton.Size = new System.Drawing.Size(75, 23);
+            this.csButton.TabIndex = 1;
+            this.csButton.Text = "C#";
+            this.csButton.UseVisualStyleBackColor = true;
+            this.csButton.Click += new System.EventHandler(this.csButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(13, 213);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 27);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(11, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Which do you think is better, Visual Basic or C#? ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.csButton);
+            this.Controls.Add(this.vbButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button vbButton;
+        private System.Windows.Forms.Button csButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ P4_4_1204014/P4_4_1204014/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace P4_4_1204014
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbnum = new System.Windows.Forms.TextBox();
+            this.tbchar = new System.Windows.Forms.TextBox();
+            this.tbregex = new System.Windows.Forms.TextBox();
+            this.tbcom1 = new System.Windows.Forms.TextBox();
+            this.tbcom2 = new System.Windows.Forms.TextBox();
+            this.tblenght = new System.Windows.Forms.TextBox();
+            this.tbupper = new System.Windows.Forms.TextBox();
+            this.tblower = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbleng = new System.Windows.Forms.TextBox();
+            this.epbenar = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epwarn = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.epbenar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epwarn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbnum
+            // 
+            this.tbnum.Location = new System.Drawing.Point(149, 12);
+            this.tbnum.Name = "tbnum";
+            this.tbnum.Size = new System.Drawing.Size(100, 20);
+            this.tbnum.TabIndex = 0;
+            this.tbnum.Leave += new System.EventHandler(this.tbnum_Leave);
+            // 
+            // tbchar
+            // 
+            this.tbchar.Location = new System.Drawing.Point(149, 38);
+            this.tbchar.Name = "tbchar";
+            this.tbchar.Size = new System.Drawing.Size(100, 20);
+            this.tbchar.TabIndex = 1;
+            this.tbchar.Leave += new System.EventHandler(this.tbchar_Leave);
+            // 
+            // tbregex
+            // 
+            this.tbregex.Location = new System.Drawing.Point(149, 64);
+            this.tbregex.Name = "tbregex";
+            this.tbregex.Size = new System.Drawing.Size(100, 20);
+            this.tbregex.TabIndex = 2;
+            this.tbregex.Leave += new System.EventHandler(this.tbregex_Leave);
+            // 
+            // tbcom1
+            // 
+            this.tbcom1.Location = new System.Drawing.Point(149, 90);
+            this.tbcom1.Name = "tbcom1";
+            this.tbcom1.Size = new System.Drawing.Size(100, 20);
+            this.tbcom1.TabIndex = 3;
+            this.tbcom1.Leave += new System.EventHandler(this.tbcom1_Leave);
+            // 
+            // tbcom2
+            // 
+            this.tbcom2.Location = new System.Drawing.Point(149, 116);
+            this.tbcom2.Name = "tbcom2";
+            this.tbcom2.Size = new System.Drawing.Size(100, 20);
+            this.tbcom2.TabIndex = 4;
+            this.tbcom2.Leave += new System.EventHandler(this.tbcom2_Leave);
+            // 
+            // tblenght
+            // 
+            this.tblenght.Location = new System.Drawing.Point(991, 286);
+            this.tblenght.Name = "tblenght";
+            this.tblenght.Size = new System.Drawing.Size(100, 20);
+            this.tblenght.TabIndex = 5;
+            // 
+            // tbupper
+            // 
+            this.tbupper.Location = new System.Drawing.Point(149, 168);
+            this.tbupper.Name = "tbupper";
+            this.tbupper.Size = new System.Drawing.Size(100, 20);
+            this.tbupper.TabIndex = 6;
+            this.tbupper.Leave += new System.EventHandler(this.tbupper_Leave);
+            // 
+            // tblower
+            // 
+            this.tblower.Location = new System.Drawing.Point(149, 194);
+            this.tblower.Name = "tblower";
+            this.tblower.Size = new System.Drawing.Size(100, 20);
+            this.tblower.TabIndex = 7;
+            this.tblower.Leave += new System.EventHandler(this.tblower_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "No. Telp";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Perbandingan";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Password";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 174);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "NAMA";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 200);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "username";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Tampilkan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbleng
+            // 
+            this.tbleng.Location = new System.Drawing.Point(149, 142);
+            this.tbleng.Name = "tbleng";
+            this.tbleng.Size = new System.Drawing.Size(100, 20);
+            this.tbleng.TabIndex = 17;
+            this.tbleng.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.tbleng.Leave += new System.EventHandler(this.tbleng_Leave);
+            // 
+            // epbenar
+            // 
+            this.epbenar.ContainerControl = this;
+            this.epbenar.Icon = ((System.Drawing.Icon)(resources.GetObject("epbenar.Icon")));
+            // 
+            // epwarn
+            // 
+            this.epwarn.ContainerControl = this;
+            this.epwarn.Icon = ((System.Drawing.Icon)(resources.GetObject("epwarn.Icon")));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 324);
+            this.Controls.Add(this.tbleng);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tblower);
+            this.Controls.Add(this.tbupper);
+            this.Controls.Add(this.tblenght);
+            this.Controls.Add(this.tbcom2);
+            this.Controls.Add(this.tbcom1);
+            this.Controls.Add(this.tbregex);
+            this.Controls.Add(this.tbchar);
+            this.Controls.Add(this.tbnum);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.epbenar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epwarn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbnum;
+        private System.Windows.Forms.TextBox tbchar;
+        private System.Windows.Forms.TextBox tbregex;
+        private System.Windows.Forms.TextBox tbcom1;
+        private System.Windows.Forms.TextBox tbcom2;
+        private System.Windows.Forms.TextBox tblenght;
+        private System.Windows.Forms.TextBox tbupper;
+        private System.Windows.Forms.TextBox tblower;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbleng;
+        private System.Windows.Forms.ErrorProvider epbenar;
+        private System.Windows.Forms.ErrorProvider epwarn;
+    }
+}
+
